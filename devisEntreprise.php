8c4ff14002e34985decedd1e3d13b87b8bfd5223
BAARTH <wanzhd@hotmail.fr>
2016-02-18 19:59:05 +0100

    testfinal

--- devisEntreprise.php
+++ devisEntreprise.php
@@ -1,110 +1,126 @@
 <!DOCTYPE html>
 
-  <html lang="fr">
 
-  <head>
-    <meta charset="utf-8">
-    <link rel="stylesheet" href="styles/style.css">
-    <link rel="stylesheet" href="webkit/stylesheet.css">
-    <link rel="stylesheet" href="styles/bootstrap.min.css" />
-    <title>Devis entreprise</title>
-  </head>
+<html lang="fr">
 
-
-
-  <body id="entreprise">
-   <header id="header">
-    <div class="bgBlank">
-      <div class="col-xs-4 col">
-        <a href="../index.html" title="accueil"><img src="images/logo_oops.png" alt="Logo oops" /></a>
-        <span class="citation">Un clic pour tout retrouver...<br>Même le sourire !</span>
-      </div>
-      <div class="col-xs-8 menu">
-        <nav>
-          <ul class="menuNormal">
-            <li><a href="#" title="devis" class="menuDevis">Contact</a>
-              <ul class="sousMenu">
-                <li><a href="../pageDevis/devisEntreprise.php" title="devisEntreprise">Entreprise</a></li>
-                <li><a href="../pageDevis/devisRevendeur.php" title="devisRevendeur">Revendeur</a></li>
-              </ul>
-            </li>
-            <li><a href="index.html" title="Aventure">Aventure</a></li>
-            <li><a href="#" title="Connexion">Espace membre</a></li>
-          </ul>
-          <ul class="menuBurger">
-            <li>
-              <div class="navbar-inverse ">
-                <button type="button" class="navbar-toggle" data-toggle="collapse" id="burger" data-target=".navbar-collapse" onclick=changerClass()>
-                  <span class="icon-bar"></span>
-                  <span class="icon-bar"></span>
-                  <span class="icon-bar"></span>
-                </button>
-              </div>
-              <ul>
-                <li><a href="#" title="connexion">Connexion</a></li>
-
-                <li><a href="index.html" title="aventure">Aventure</a></li>
-                <li><a href="#" title="devis" class="menuDevis">Devis</a>
-                  <ul class="sousMenu">
-                    <li><a href="../pageDevis/devisEntreprise.php" title="devisEntreprise">Entreprise</a></li>
-                    <li><a href="../pageDevis/devisRevendeur.php" title="devisRevendeur">Revendeur</a></li>
-                  </ul>
-                </li>
-              </ul>
-            </li>
-          </ul>
-        </nav>
-      </div>
-    </div>
-  </header>
-    <div id="map">
-
-      <p id="entreprise">Faites comme eux,
-        <br>travaillez avec nous !</p>
-
-    </div>
-
-    <div class="col-xs-12 col-sm-12 col-md-12" id="forme">
-      <h1>Entreprises</h1>
-
-      <form action="../index.html" method="post">
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<meta name="viewport" content="width=device-width; initial-scale=1.0; maximum-scale=1.0; user-scalable=0;" />
+	<link rel="stylesheet" href="styles/bootstrap.min.css" />
+	<link rel="stylesheet" href="styles/devis.css">
+	<link rel="stylesheet" href="webkit/stylesheet.css">
+	<title>Devis Entreprises</title>
+</head>
 
 
+<body id="entreprise">
+	<header id="header">
+		<div class="bgBlank">
+			<div class="col-xs-4 col">
+				<a href="index.html" title="accueil"><img src="images/logo_oops.png" alt="Logo oops" /></a>
+				<span class="citation">Un clic pour tout retrouver...<br>Même le sourire !</span>
+			</div>
+			<div class="col-xs-8 menu">
+				<nav>
+					<ul class="menuNormal">
+						<li><a href="#" title="devis" class="menuDevis">Contact</a>
+							<ul class="sousMenu">
+								<li><a href="devisEntreprise.php" title="devisEntreprise">Entreprise</a></li>
+								<li><a href="devisRevendeur.php" title="devisRevendeur">Revendeur</a></li>
+							</ul>
+						</li>
+						<li><a href="index.html" title="Aventure">Aventure</a></li>
+						<li><a href="#" title="Connexion">Espace membre</a></li>
+					</ul>
+					<ul class="menuBurger">
+						<li>
+							<div class="navbar-inverse ">
+								<button type="button" class="navbar-toggle" data-toggle="collapse" id="burger" data-target=".navbar-collapse" onclick=changerClass()>
+									<span class="icon-bar"></span>
+									<span class="icon-bar"></span>
+									<span class="icon-bar"></span>
+								</button>
+							</div>
+							<ul>
+								<li><a href="#" title="connexion">Connexion</a></li>
 
-        <label for="nom" class="initLabel">Bonjour, je m'appelle</label>
-        <input type="text" name="nom" class="initInput" />
-        <br>
+								<li><a href="index.html" title="aventure">Aventure</a></li>
+								<li><a href="#" title="devis" class="menuDevis">Contact</a>
+									<ul class="sousMenu">
+										<li><a href="devisEntreprise.php" title="devisEntreprise">Entreprise</a></li>
+										<li><a href="devisRevendeur.php" title="devisRevendeur">Revendeur</a></li>
+									</ul>
+								</li>
+							</ul>
+						</li>
+					</ul>
+				</nav>
+			</div>
+		</div>
+	</header>
+		<section id="contenu">
+	<div id="map">
 
-        <label for="entreprise" class="labelDevis">mon entreprise est </label>
-        <input type="text" name="entreprise" class="inputDevis" />
-        <br>
+		<p id="entreprise">Faites comme eux,
+			<br>travaillez avec nous !</p>
 
-        <label for="ville" class="labelDevis">et est localisée à </label>
-        <input type="text" name="ville" class="inputDevis" />
-        <br>
+	</div>
 
-        <label for="salarie" class="labelDevis">Le nombre de salarié est de </label>
-        <input type="number" name="salarie" class="inputDevis" />
-        <br>
+	<div id="forme">
+		<h1>Entreprises</h1>
 
-        <label for="tel" class="labelDevis">Voici mon numéro de téléphone </label>
-        <input type="number" name="tel" class="inputDevis" />
-        <br>
-
-        <label for="mail" class="labelDevis">et mon email</label>
-        <input type="email" name="mail" class="inputDevis" />
-        <br>
-
-        <label for="comment" class="labelDevis">Je voudrai rajouter</label>
-        <input type="text" name="comment" class="inputDevis" />
-
-
-        <input type="submit" name="send" value="SUBMIT" class="bouton" id="entreprise" />
-      </form>
-    </div>
+		<form action="index.html" method="post">
 
 
-    <script src="scripts/main.js"></script>
-  </body>
 
-  </html>
+			<label for="nom" class="initLabel">Bonjour, je m'appelle</label>
+			<input type="text" name="nom" class="initInput" />
+			<br>
+
+			<label for="entreprise" class="labelDevis">mon entreprise est </label>
+			<input type="text" name="entreprise" class="inputDevis" />
+			<br>
+
+			<label for="ville" class="labelDevis">et est localisée à </label>
+			<input type="text" name="ville" class="inputDevis" />
+			<br>
+
+			<label for="salarie" class="labelDevis">Le nombre de salarié est de </label>
+			<input type="number" name="salarie" class="inputDevis" />
+			<br>
+
+			<label for="tel" class="labelDevis">Voici mon numéro de téléphone </label>
+			<input type="number" name="tel" class="inputDevis" />
+			<br>
+
+			<label for="mail" class="labelDevis">et mon email</label>
+			<input type="email" name="mail" class="inputDevis" />
+			<br>
+
+			<label for="comment" class="labelDevis">Je voudrai rajouter</label>
+			<input type="text" name="comment" class="inputDevis" />
+
+
+			<input type="submit" name="send" value="SUBMIT" class="bouton" id="entreprise" />
+		</form>
+	</div>
+	</section>
+	
+	<footer>
+		<div id="all" class=" col-md-offset-1 col-md-10 col-sm-offset-1 col-sm-10 col-xs-12">
+			<div class="footcol-md-2 col-sm-2 col-xs-4"><a href="#" title="suivez-nous">Blog</a></div>
+			<div class="foot col-md-2 col-sm-2 col-xs-4"><a href="#" title="remerciement">Merci !</a></div>
+			<div class="foot col-md-2 col-sm-2 col-xs-4"><a href="#" title="FAQ">FAQ</a></div>
+			<div class="foot col-md-2 col-sm-2 col-xs-4"><a href="#" title="contact">Contact</a></div>
+			<div class="foot col-md-2 col-sm-2 col-xs-4"><a href="#" title="CVG">CVG</a></div>
+			<div class="foot col-md-2 col-sm-2 col-xs-4"><a href="#" title="mention">Mention</a></div>
+		</div>
+
+		<div class="clearBoth"></div>
+	</footer>
+	
+	<script src="scripts/devis.js"></script>
+</body>
+
+</html>
--- scripts/aventure.js
+++ scripts/aventure.js
@@ -56,8 +56,8 @@ function entreprise() {
   position = position - 2.5;
   scooter.style.transform = "ScaleX(-1)"
   scooter.style.left = position + 'px';
-  setTimeout(entreprise, 0.1);
-  if (position < -200) {
+  setTimeout(entreprise,0.1);
+  if (position < -200){
     window.location.href = "file:///C:/Users/ybaun/Desktop/Semaine%20intensive/Site%20Web/entreprise.html";
   }
 }
@@ -65,16 +65,43 @@ function entreprise() {
 function revendeur() {
   position = position + 2.5;
   scooter.style.left = position + 'px';
-  setTimeout(revendeur, 0.1);
-  if (position > 1920) {
+  setTimeout(revendeur,0.1);
+  if (position > 1920){
     window.location.href = "file:///C:/Users/ybaun/Desktop/Semaine%20intensive/Site%20Web/revendeur.html";
   }
 }
 
 /************
 
-skroll
+Animsition
 
 ************/
+
+    $(document).ready(function () {
+      $(".animsition").animsition({
+        inClass: 'fade-in-left',
+        outClass: 'fade-out-left',
+        inDuration: 1500,
+        outDuration: 800,
+        linkElement: '.animsition-link',
+        // e.g. linkElement: 'a:not([target="_blank"]):not([href^=#])'
+        loading: true,
+        loadingParentElement: 'body', //animsition wrapper element
+        loadingClass: 'animsition-loading',
+        loadingInner: '', // e.g '<img src="loading.svg" />'
+        timeout: false,
+        timeoutCountdown: 5000,
+        onLoadEvent: true,
+        browser: ['animation-duration', '-webkit-animation-duration'],
+        // "browser" option allows you to disable the "animsition" in case the css property in the array is not supported by your browser.
+        // The default setting is to disable the "animsition" in a browser that does not support "animation-duration".
+        overlay: false,
+        overlayClass: 'animsition-overlay-slide',
+        overlayParentElement: 'body',
+        transition: function (url) {
+          window.location.href = url;
+        }
+      });
+    });
 
 var s = skrollr.init();
